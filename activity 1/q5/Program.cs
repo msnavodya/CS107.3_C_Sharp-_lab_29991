8beda41110fffb1ae720720915cd660ed18aa093
msnavodya <135606838+msnavodya@users.noreply.github.com>
2024-02-20 14:23:49 +0530

    Add files via upload

---
+++ activity 1/q5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Multiplication table for {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
---
+++ activity 1/q6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+ 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" Question 6 \n");
+
+        Console.Write("Enter a Name : ");
+        String name = Console.ReadLine();
+        String Grade = "";
+
+        Console.Write("Enter Marks : ");
+        int marks = int.Parse(Console.ReadLine());
+
+        if (marks > 100)
+        {
+            Console.WriteLine("Wrong MArks !");
+        }
+        else if (marks >= 75)
+        {
+            Grade = "A";
+        }
+        else if (marks >= 65)
+        {
+            Grade = "B";
+        }
+        else if (marks >= 55)
+        {
+            Grade = "C";
+        }
+        else if (marks >= 45)
+        {
+            Grade = "D";
+        }
+        else
+        {
+            Console.WriteLine("Faild");
+        }
+        Console.WriteLine(name + " Your Grade is " + Grade);
+
+        Console.ReadLine();
+    }
+}
